de8b95952731715affebcf0d9580b826ebfa9191
Quinton Ashley <qashto@gmail.com>
2021-09-18 01:37:51 -0500

    1.0.1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const log = console.log; // shortcut
+
 // enable popovers
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
@@ -10,9 +12,8 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 });
 
 $(() => {
-	let clothingOptions = [
-		'All',
-		'Clothing',
+	let clothesOptions = [
+		'All Clothing',
 		'Beachwear',
 		'Blazers',
 		'Coats',
@@ -32,12 +33,48 @@ $(() => {
 		'Tops'
 	];
 
-	let $clothesMenu = $('#clothesMenu');
-	let $cols = $clothesMenu.find('.col');
+	let accessoryOptions = [
+		'All accessories',
+		'Belts',
+		'Hair accessories',
+		'Jewelry/Chains',
+		'Gloves',
+		'Scarves',
+		'Sunglasses',
+		'Watches'
+	];
 
-	for (let i = 0; i < clothingOptions.length; i++) {
-		let opt = clothingOptions[i];
-		let colNum = i % 3;
-		$cols.eq(colNum).append('<div class="row"><a href="#" class="col">' + opt + '</a></div>');
+	let shoesOptions = ['All shoes', 'Boots', 'Espadrilles', 'Heels', 'Sneakers', 'Sandals'];
+
+	let bagsOptions = ['All bags', 'Backpacks', 'Chain bags', 'Clutches'];
+
+	function addColumns(menu, options) {
+		let $menu = $('#' + menu + 'Menu');
+		$menu.append(`
+<div class="row">
+	<div class="col-8">
+		<div class="row">
+			<div class="col-12 fs-3 green">Shop ${menu[0].toUpperCase() + menu.slice(1)}</div>
+			<div class="col"></div>
+			<div class="col"></div>
+			<div class="col"></div>
+		</div>
+	</div>
+	<div class="col-4">
+		images
+	</div>
+</div>`);
+		let $cols = $menu.find('.col');
+
+		for (let i = 0; i < options.length; i++) {
+			let opt = options[i];
+			let colNum = Math.floor((i / options.length) * 3);
+			$cols.eq(colNum).append('<div class="row"><a href="#" class="col">' + opt + '</a></div>');
+		}
 	}
+
+	addColumns('clothes', clothesOptions);
+	addColumns('shoes', shoesOptions);
+	addColumns('bags', bagsOptions);
+	addColumns('accessories', accessoryOptions);
 });
