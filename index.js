0fe717beef04d482c07ac981baab9456e47c3bcf
Valencia-Hartono <valenciahartono@gmail.com>
2021-09-21 12:49:18 +0800

    1.0.2

--- index.js
+++ index.js
@@ -12,52 +12,49 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 });
 
 $(() => {
-	let clothesOptions = [
-		'All Clothing',
-		'Beachwear',
-		'Blazers',
-		'Coats',
-		'Denim',
-		'Dresses',
-		'Jackets',
-		'Jeans',
-		'Jumpsuits',
-		'Leather',
-		'Loungewear',
-		'Pants',
-		'Shorts',
-		'Skirts',
-		'Sportswear',
-		'Sweater',
-		'Swimwear',
-		'Tops'
-	];
+	// superCategories(clothing, accessories, shoes, bags), categories (ex.bottoms, tops, overalls), subCategories (ex.jeans, shorts...)
+	let superCategories = {
+		clothing: {
+			Bottoms: ['Jeans', 'Shorts', 'Skirts', 'Slacks', 'Sweatpants'],
+			Tops: ['Shirts', 'Blazers', 'Coats', 'Hoodies', 'Jackets', 'Sweater'],
+			Overalls: ['Beachwear', 'Dresses', 'Jumpsuits', 'Loungewear']
+		},
+		accessories: {
+			Jewelry: ['Earrings', 'Bracelets', 'Necklaces'],
+			Belts: [],
+			'Hair accessories': [],
+			Gloves: [],
+			Scarves: [],
+			Sunglasses: [],
+			Watches: []
+		},
+		shoes: {
+			Boots: ['Leather', 'k', 'l'],
+			Heels: ['k', 'a'],
+			Formal: ['a', 'b'],
+			Casual: ['Espadrilles'],
+			r: ['k', 'f']
+		},
+		bags: {
+			Backpacks: [],
+			'Chain bags': [],
+			Clutches: [],
+			'Duffle bags': [],
+			'Shoulder bags': [],
+			'Tote bags': []
+		}
+	};
 
-	let accessoryOptions = [
-		'All accessories',
-		'Belts',
-		'Hair accessories',
-		'Jewelry/Chains',
-		'Gloves',
-		'Scarves',
-		'Sunglasses',
-		'Watches'
-	];
-
-	let shoesOptions = ['All shoes', 'Boots', 'Espadrilles', 'Heels', 'Sneakers', 'Sandals'];
-
-	let bagsOptions = ['All bags', 'Backpacks', 'Chain bags', 'Clutches'];
-
-	function addColumns(menu, options) {
-		let $menu = $('#' + menu + 'Menu');
+	function addColumns(superCategory) {
+		let $menu = $('#' + superCategory + 'Menu');
 		$menu.append(`
 <div class="row">
-	<div class="col-8">
+	<div class="col-8 px-4">
 		<div class="row">
-			<div class="col-12 fs-3 green">Shop ${menu[0].toUpperCase() + menu.slice(1)}</div>
-			<div class="col"></div>
-			<div class="col"></div>
-			<div class="col"></div>
+			<div class="col-12 fs-4 green">Shop ${superCategory[0].toUpperCase() + superCategory.slice(1)}</div>
+			<div class="col"> _____________ </div>
+			<div class="col"> _____________ </div>
+			<div class="col"> _____________ </div>
 		</div>
 	</div>
 	<div class="col-4">
@@ -65,16 +62,45 @@ $(() => {
 	</div>
 </div>`);
 		let $cols = $menu.find('.col');
+		let $superCategories = $('#superCategories');
+		$superCategories.append('<div id="' + superCategory + 'Sidebar" class="row"></div>'); //ex. id=clothingSidebar
+		let $superCategory = $('#' + superCategory + 'Sidebar'); // retrieves the element
+		$superCategory.hide();
+		let categories = Object.keys(superCategories[superCategory]); // returns an array of category names ex. tops, bottoms
 
-		for (let i = 0; i < options.length; i++) {
-			let opt = options[i];
-			let colNum = Math.floor((i / options.length) * 3);
-			$cols.eq(colNum).append('<div class="row"><a href="#" class="col">' + opt + '</a></div>');
+		let cols = [0, 0, 0];
+
+		for (let i = 0; i < categories.length; i++) {
+			//loops through array of category names
+			let category = categories[i]; //retrieves a category name (ex.top or bottom)
+
+			// find which column has the least amount of items
+			// place the category and sub-cat options in that column
+			let colNumMin = 0;
+			for (let i = 1; i < cols.length; i++) {
+				if (cols[colNumMin] > cols[i]) {
+					colNumMin = i;
+				}
+			}
+
+			$cols.eq(colNumMin).append('<div class="row"><a href="#" class="col">' + category + '</a></div>');
+			$superCategory.append('<a href="#" class="col-12">' + category + '</a>');
+
+			let subCats = superCategories[superCategory][category]; // retrieves the array of the category names
+			for (let j = 0; j < subCats.length; j++) {
+				$cols.eq(colNumMin).append('<div class="row"><a href="#" class="col"> - ' + subCats[j] + '</a></div>');
+				$superCategory.append('<a href="#" class="col-12"> - ' + subCats[j] + '</a>');
+			}
+			cols[colNumMin] += subCats.length + 1;
 		}
 	}
 
-	addColumns('clothes', clothesOptions);
-	addColumns('shoes', shoesOptions);
-	addColumns('bags', bagsOptions);
-	addColumns('accessories', accessoryOptions);
+	addColumns('clothing');
+	addColumns('shoes');
+	addColumns('bags');
+	addColumns('accessories');
+
+	$('#clothingSidebar').show();
+
+	$('#shoes').show();
 });
